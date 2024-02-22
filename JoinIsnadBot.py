ea77c6c9dcbe6d7668415293eea4c423da838523
mmansour2 <160733051+mmansour2@users.noreply.github.com>
2024-02-22 08:40:11 +0300

    Update JoinIsnadBot.py
    New API Token

--- JoinIsnadBot.py
+++ JoinIsnadBot.py
@@ -236,7 +236,7 @@ def cancel(update: Update, context: CallbackContext) -> int:
 def main() -> None:
     """Run the bot."""
     # Create the Updater and pass it your bot's token
-    updater = Updater("6918060750:AAEUR6Jwoy9JxiEoDyIUyFEb45JR1jaDwhE")
+    updater = Updater("6918060750:AAHwR76weMCoiK4GxiHxdbaFp81m8k2r9XA")
 
     # Get the dispatcher to register handlers
     dispatcher = updater.dispatcher
